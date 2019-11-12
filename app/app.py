f44c3a6ced2851345ba8f074d8837749a9ab7f7a
Anumita Shenoy <ansheno@microsoft.com>
2019-11-12 19:39:57 +0530

    datadog

--- app/app.py
+++ app/app.py
@@ -2,23 +2,30 @@
 
 from random import randrange
 from flask import Flask
-from prometheus_client import start_http_server, Counter
+from datadog import initialize
+from datadog import api
+from datadog import statsd
 import os
-import sys
+import datetime
 
-app = Flask('bootcamp-demo')
-c = Counter('requests', 'Number of requests served, by custom_status', ['custom_status'])
+options = {
+    'api_key':'b18988cfea6d6f52dc82532ccaedd1c6',
+    'app_key':'4d54ff31bfbb572d9988f037664b5e977b75221b'
+}
+
+initialize(**options)
+
+app = Flask('doggopy-demo')
+appVersion = os.getenv('APP_VERSION', "1")
+tags = ['appVersion:{}'.format(appVersion)]
 
 @app.route('/')
 def hello():
     if randrange(1, 100) > 40: # 90% of the requests are "good"
-        c.labels(custom_status = 'bad').inc()
-        print("bad request", file=sys.stderr)
+        api.Metric.send(metric="app.custom_status", points=[datetime.datetime.utcnow().timestamp(), 1], host=os.getenv('COMPUTERNAME'), tags=tags, type="count")
         return "Internal Server Error\n", 500
     else:
-        c.labels(custom_status = 'good').inc()
-        print "good request"
+        api.Metric.send(metric="app.custom_status", points=[datetime.datetime.utcnow().timestamp(), 0], host=os.getenv('COMPUTERNAME'), tags=tags, type="count")
         return "Hello World!\n", 200
 
-start_http_server(8000)
 app.run(host = '0.0.0.0', port = 8080)
